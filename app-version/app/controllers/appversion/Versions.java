cba5de13446d200d74cc3f8fe1159c342072bb98
crazycode <crazycode@gmail.com>
2012-04-11 18:06:34 +0800

    实现plugin功能

---
+++ app-version/app/controllers/appversion/Versions.java
@@ -0,0 +1,16 @@
+package controllers.appversion;
+
+import play.AppVersion;
+import play.mvc.Controller;
+
+public class Versions extends Controller {
+
+    public static void index() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(AppVersion.name).append(" ").append(AppVersion.name)
+            .append("\n").append("revision:").append(AppVersion.revision)
+            .append("\n").append("Build At:").append(AppVersion.buildAt)
+            .append("\n").append("StartUp:").append(AppVersion.startupAt);
+        renderText(sb.toString());
+    }
+}
