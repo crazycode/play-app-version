14edd8e67fb76122fb077d08884b440fa026e705
crazycode <crazycode@gmail.com>
2012-04-11 19:12:16 +0800

    解决没有显示Appversion.value的问题

--- app-version/app/controllers/appversion/Versions.java
+++ app-version/app/controllers/appversion/Versions.java
@@ -7,7 +7,7 @@ public class Versions extends Controller {
 
     public static void index() {
         StringBuilder sb = new StringBuilder();
-        sb.append(AppVersion.name).append(" ").append(AppVersion.name)
+        sb.append(AppVersion.name).append(" ").append(AppVersion.value)
             .append("\n").append("revision:").append(AppVersion.revision)
             .append("\n").append("Build At:").append(AppVersion.buildAt)
             .append("\n").append("StartUp:").append(AppVersion.startupAt);
