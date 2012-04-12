436f24264e123cf1317245c133f28bd48451b518
crazycode <crazycode@gmail.com>
2012-04-12 10:14:25 +0800

    调整时间显示格式

--- app-version/app/controllers/appversion/Versions.java
+++ app-version/app/controllers/appversion/Versions.java
@@ -2,15 +2,17 @@ package controllers.appversion;
 
 import play.AppVersion;
 import play.mvc.Controller;
+import java.text.SimpleDateFormat;
 
 public class Versions extends Controller {
 
     public static void index() {
         StringBuilder sb = new StringBuilder();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         sb.append(AppVersion.name).append(" ").append(AppVersion.value)
             .append("\n").append("revision:").append(AppVersion.revision)
-            .append("\n").append("Build At:").append(AppVersion.buildAt)
-            .append("\n").append("StartUp:").append(AppVersion.startupAt);
+            .append("\n").append("Build At:").append(sdf.format(AppVersion.buildAt))
+            .append("\n").append("StartUp:").append(sdf.format(AppVersion.startupAt));
         renderText(sb.toString());
     }
 }
